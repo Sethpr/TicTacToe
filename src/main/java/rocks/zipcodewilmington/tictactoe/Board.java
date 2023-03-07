5432acda4152dff10a3c9728bd7f2f235635d675
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-03-07 14:08:36 -0500

    passed all tests

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,97 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
-    public Boolean isInFavorOfX() {
-        return null;
+    public Boolean isInFavorOfX() { //on the off chance someone reads this, I would like to state I hate 2d arrays
+        int horizontal = 0;
+        int vertical = 0;
+        int diagonal = 0;
+        int reverseDiagonal = 0;
+        for(int i = 0; i< matrix.length; i++){
+            if(matrix[i][i] == 'X'){
+                diagonal++;
+            }
+            if(matrix[i][matrix.length - i - 1] == 'X'){
+                reverseDiagonal++;
+            }
+            for(int j = 0; j < matrix.length; j++){
+                if(matrix[j][i] == 'X'){
+                    vertical++;
+                }
+                if(matrix[i][j] == 'X'){
+                    horizontal++;
+                }
+            }
+            if(vertical == 3 || horizontal == 3){
+                return true;
+            }
+            horizontal = 0;
+            vertical = 0;
+        }
+
+
+        if(diagonal == 3 || reverseDiagonal == 3){
+            return true;
+        }
+
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        int horizontal = 0;
+        int vertical = 0;
+        int diagonal = 0;
+        int reverseDiagonal = 0;
+        for(int i = 0; i< matrix.length; i++){
+            if(matrix[i][i] == 'O'){
+                diagonal++;
+            }
+            if(matrix[i][matrix.length - i - 1] == 'O'){
+                reverseDiagonal++;
+            }
+            for(int j = 0; j < matrix.length; j++){
+                if(matrix[j][i] == 'O'){
+                    vertical++;
+                }
+                if(matrix[i][j] == 'O'){
+                    horizontal++;
+                }
+            }
+            if(vertical == 3 || horizontal == 3){
+                return true;
+            }
+            horizontal = 0;
+            vertical = 0;
+        }
+
+
+        if(diagonal == 3 || reverseDiagonal == 3){
+            return true;
+        }
+
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        return !isInFavorOfO() && !isInFavorOfX();
     }
 
     public String getWinner() {
-        return null;
+        if(isTie()){
+            return "";
+        }
+        else if(isInFavorOfO()){
+            return "O";
+        }
+        else if(isInFavorOfX()){
+            return "X";
+        }
+
+        return "?";
     }
 
 }
